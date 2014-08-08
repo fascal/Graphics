4be207e9b4962c6e54fe2e1d09ad9f6fc5e6e9d5
Hongbo Fang <ffhongbo@gmail.com>
2014-08-07 20:44:09 -0400

    displayed

--- graphics/GLSLExperiment/example1.cpp
+++ graphics/GLSLExperiment/example1.cpp
@@ -2,6 +2,7 @@
 
 #include "Angel.h"
 
+using namespace std;
 
 int width = 0;
 int height = 0;
@@ -10,82 +11,98 @@ void display( void );
 void keyboard( unsigned char key, int x, int y );
 void init(int argc, char **argv);
 void addToBuffer(vec4 *vertecies, vec4 *colors, int nfaces);
+void enableBuffer(int index, int size);
+void allocateMatrix(GLfloat* array, Angel::mat4 matrix);
+vec4* allocRandomColor(int nfaces);
+void loadSTLfile(string _filename, vec4** _facebuffer, int* _nfacebuffer);
 
 GLuint g_program;
 
-using namespace std;
+
 //GLuint buffer[2];
 
 GLuint *g_buffers;
 int g_bufferSize;
 int g_allocatedBufferSize;
-void display( void )
-{
 
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );     // clear the window
+int g_nfaces;
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	Angel::mat4 perspectiveMat = Angel::Perspective((GLfloat)45.0, (GLfloat)width/(GLfloat)height, (GLfloat)0.1, (GLfloat) 100.0);
+	Angel::mat4 perspectiveMat = Angel::Perspective((GLfloat)45.0, (GLfloat)width / (GLfloat)height, (GLfloat)0.1, (GLfloat) 100.0);
 
 	float viewMatrixf[16];
-	viewMatrixf[0] = perspectiveMat[0][0];viewMatrixf[4] = perspectiveMat[0][1];
-	viewMatrixf[1] = perspectiveMat[1][0];viewMatrixf[5] = perspectiveMat[1][1];
-	viewMatrixf[2] = perspectiveMat[2][0];viewMatrixf[6] = perspectiveMat[2][1];
-	viewMatrixf[3] = perspectiveMat[3][0];viewMatrixf[7] = perspectiveMat[3][1];
-
-	viewMatrixf[8] = perspectiveMat[0][2];viewMatrixf[12] = perspectiveMat[0][3];
-	viewMatrixf[9] = perspectiveMat[1][2];viewMatrixf[13] = perspectiveMat[1][3];
-	viewMatrixf[10] = perspectiveMat[2][2];viewMatrixf[14] = perspectiveMat[2][3];
-	viewMatrixf[11] = perspectiveMat[3][2];viewMatrixf[15] = perspectiveMat[3][3];
-	
+	allocateMatrix(viewMatrixf, perspectiveMat);
 	Angel::mat4 modelMat = Angel::identity();
-	modelMat = modelMat * Angel::Translate(0.0, 0.0, -2.0f) * Angel::RotateY(45.0f) * Angel::RotateX(35.0f);
+	float s = 1;
+	modelMat = modelMat * Angel::Translate(0.0, 0.0, -10.0f) * Angel::RotateY(45.0f) * Angel::RotateX(35.0f) * Angel::Scale(s, s, s);
 	float modelMatrixf[16];
-	modelMatrixf[0] = modelMat[0][0];modelMatrixf[4] = modelMat[0][1];
-	modelMatrixf[1] = modelMat[1][0];modelMatrixf[5] = modelMat[1][1];
-	modelMatrixf[2] = modelMat[2][0];modelMatrixf[6] = modelMat[2][1];
-	modelMatrixf[3] = modelMat[3][0];modelMatrixf[7] = modelMat[3][1];
+	allocateMatrix(modelMatrixf, modelMat);
 
-	modelMatrixf[8] = modelMat[0][2];modelMatrixf[12] = modelMat[0][3];
-	modelMatrixf[9] = modelMat[1][2];modelMatrixf[13] = modelMat[1][3];
-	modelMatrixf[10] = modelMat[2][2];modelMatrixf[14] = modelMat[2][3];
-	modelMatrixf[11] = modelMat[3][2];modelMatrixf[15] = modelMat[3][3];
-	
-	// set up projection matricies
 	GLuint modelMatrix = glGetUniformLocationARB(g_program, "model_matrix");
 	glUniformMatrix4fv( modelMatrix, 1, GL_FALSE, modelMatrixf );
 	GLuint viewMatrix = glGetUniformLocationARB(g_program, "projection_matrix");
 	glUniformMatrix4fv( viewMatrix, 1, GL_FALSE, viewMatrixf);
 
-    glFlush(); // force output to graphics hardware
+	glFlush();
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glEnable(GL_DEPTH_TEST);
+	
+	enableBuffer(0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, g_nfaces);
 
-	glBindBuffer(GL_ARRAY_BUFFER, g_buffers[0]);
+	glDisable(GL_DEPTH_TEST);
+	glutSwapBuffers();
+
+}
+//void display( void )
+//{
+//
+//    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );     // clear the window
+//
+//	Angel::mat4 perspectiveMat = Angel::Perspective((GLfloat)45.0, (GLfloat)width/(GLfloat)height, (GLfloat)0.1, (GLfloat) 100.0);
+//
+//	float viewMatrixf[16];
+//	allocateMatrix(viewMatrixf, perspectiveMat);
+//	
+//	Angel::mat4 modelMat = Angel::identity();
+//	modelMat = modelMat * Angel::Translate(0.0, 0.0, -2.0f) * Angel::RotateY(45.0f) * Angel::RotateX(35.0f);
+//	float modelMatrixf[16];
+//	allocateMatrix(modelMatrixf, modelMat);
+//	
+//	// set up projection matricies
+//	GLuint modelMatrix = glGetUniformLocationARB(g_program, "model_matrix");
+//	glUniformMatrix4fv( modelMatrix, 1, GL_FALSE, modelMatrixf );
+//	GLuint viewMatrix = glGetUniformLocationARB(g_program, "projection_matrix");
+//	glUniformMatrix4fv( viewMatrix, 1, GL_FALSE, viewMatrixf);
+//
+//    glFlush(); // force output to graphics hardware
+//
+//	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//	glEnable(GL_DEPTH_TEST);
+//
+//	enableBuffer(0, 1);
+//	glDrawArrays(GL_TRIANGLES, 0, 3);
+//
+//	enableBuffer(1, 1);
+//	glDrawArrays(GL_TRIANGLES, 0, 3);
+//	glDisable(GL_DEPTH_TEST);
+//	// use this call to double buffer
+//	glutSwapBuffers();
+//	// you can implement your own buffers with textures
+//}
+
+void enableBuffer(int index, int size) {
+	glBindBuffer(GL_ARRAY_BUFFER, g_buffers[index]);
 	GLuint vPosition = glGetAttribLocation(g_program, "vPosition");
 	glEnableVertexAttribArray(vPosition);
 	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 
 	GLuint vColor = glGetAttribLocation(g_program, "vColor");
 	glEnableVertexAttribArray(vColor);
-	glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(vec4)* 3));
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-
-	glBindBuffer(GL_ARRAY_BUFFER, g_buffers[1]);
-	vPosition = glGetAttribLocation(g_program, "vPosition");
-	glEnableVertexAttribArray(vPosition);
-	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-
-	vColor = glGetAttribLocation(g_program, "vColor");
-	glEnableVertexAttribArray(vColor);
-	glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(vec4)* 3));
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDisable(GL_DEPTH_TEST);
-	// use this call to double buffer
-	glutSwapBuffers();
-	// you can implement your own buffers with textures
+	glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(vec4)* 3 * size));
 }
-
 // keyboard handler
 void keyboard( unsigned char key, int x, int y )
 {
@@ -108,9 +125,10 @@ void init(int argc, char **argv) {
 
 	glewInit();
 
+
 }
 
-void loadSTLFile() {
+void genTestTriangle() {
 	int nfaces = 3;
 	vec4 *vertecies = (vec4*)malloc(sizeof(vec4)* nfaces);
 	vec4 *colors = (vec4*)malloc(sizeof(vec4)* nfaces);
@@ -147,12 +165,52 @@ void loadSTLFile() {
 
 }
 
+void initBuffer(int buffer_size) {
+
+	GLuint *vao;
+	glGenVertexArrays(2, vao);
+	glBindVertexArray(vao[0]);
+
+	g_buffers = (GLuint*)malloc(sizeof(GLuint)* buffer_size);
+	g_allocatedBufferSize = buffer_size;
+	g_bufferSize = 0;
+
+	glGenBuffers(buffer_size, g_buffers);
+}
+
+void loadVertecies() {
+	int nfaces;
+	vec4 *vertecies;
+	loadSTLfile("liver.stl", &vertecies, &nfaces);
+	g_nfaces = nfaces;
+	vec4 *colors = allocRandomColor(nfaces);
+
+	initBuffer(1);
+
+	g_program = InitShader("vshader1.glsl", "fshader1.glsl");
+	glUseProgram(g_program);
+
+	addToBuffer(vertecies, colors, nfaces);
+
+	//for (int i = 0; i < 10; i++) {
+	//	cout << vertecies[i] << endl;
+	//}
+}
+
+vec4* allocRandomColor(int nfaces) {
+	vec4 *colors = (vec4*)malloc(sizeof(vec4)* nfaces * 3);
+	for (int i = 0; i < nfaces * 3; i++) {
+		colors[i] = (float)rand() / RAND_MAX;
+	}
+
+	return colors;
+}
 int main(int argc, char **argv) {
-	
+	srand(1);
 	init(argc, argv);
 	
-	loadSTLFile();
-
+	//genTestTriangle();
+	loadVertecies();
 	glClearColor(1, 1, 1, 1);
 
 	glutDisplayFunc(display);
@@ -176,4 +234,66 @@ void addToBuffer(vec4 *vertecies, vec4 *colors, int nfaces) {
 	else {
 		cout << "exceed buffer size limit" << endl;
 	}
+}
+
+void allocateMatrix(GLfloat* array, Angel::mat4 matrix) {
+	int i = 0, j = 0, m;
+	for (m = 0; m < 16; m++) {
+		array[m] = matrix[i][j];
+		i++;
+		if (i == 4) {
+			i = 0;
+			j++;
+		}
+	}
+}
+
+void loadSTLfile(string _filename, vec4** _facebuffer, int* _nfacebuffer) {
+	int filenamelength = _filename.length();
+	char *pathname = (char *)malloc(sizeof(char)* filenamelength);
+	for (int i = 0; i < filenamelength; i++) {
+		pathname[i] = _filename[i];
+	}
+	pathname[filenamelength] = '\0';
+	int TriangleNum;
+
+	FILE *fp = fopen(pathname, "rb");
+	printf("%s\n", pathname);
+	fseek(fp, 80 * sizeof(char), 0);
+	fread(&TriangleNum, sizeof(int), 1, fp);
+	cout << TriangleNum << endl;
+	float data;
+	float col[2];
+
+	float **Triangle = (float**)malloc(sizeof(float*)* 12);
+
+	for (int j = 0; j < 12; j++)
+	{
+		Triangle[j] = new float[TriangleNum];
+	}
+
+	for (int i = 0; i < TriangleNum; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			fread(&data, sizeof(float), 1, fp);
+			Triangle[j][i] = data;
+		}
+		fread(&col, sizeof(char), 2, fp);
+	}
+	fclose(fp);
+
+	vec4* facebuffer = (vec4*)malloc(sizeof(vec4)* TriangleNum * 3);
+	for (int i = 0; i < TriangleNum; i++) {
+		vec4 points[3];
+		for (int j = 0; j < 3; j++) {
+			points[j] = vec4(Triangle[j * 3 + 3][i] / 200, Triangle[j * 3 + 4][i] / 200, Triangle[j * 3 + 5][i] / 200, 1);
+		}
+		facebuffer[i * 3] = points[0];
+		facebuffer[i * 3 + 1] = points[1];
+		facebuffer[i * 3 + 2] = points[2];
+	}
+
+	*_facebuffer = facebuffer;
+	*_nfacebuffer = TriangleNum;
 }
